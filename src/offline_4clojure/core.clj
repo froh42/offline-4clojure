1e73dbb5f1afa30fe26e53c8a313342627cc4c7c
Frohwalt Egerer <mail@frohwalt.de>
2016-02-09 12:08:12 +0100

    Add form-zip to replace __ in complete form

--- src/offline_4clojure/core.clj
+++ src/offline_4clojure/core.clj
@@ -2,12 +2,13 @@
   (:use clojure.test)
   (:use clojure.pprint)
   (:use clojure.walk)
-  (:require [clojure.zip :as zip]))
+  (:require [clojure.zip :as zip])
+  (:require [offline-4clojure.form-zip :as fz]))
 
 ; for editing the tree
 (defn fill-in-solution [template solution]
   ; loop/recur durch alle Elemente des Tree, fange mit dem zipseq an, mache dann mit der jeweils nächsten loc weiter.
-  (loop [loc (zip/seq-zip (seq template))]
+  (loop [loc (fz/form-zip template)]
     ; Ende? Dann gib den Ergebnisbaum aus
     (if (zip/end? loc)
       (zip/root loc)
@@ -15,18 +16,19 @@
       (recur
         ; Nächstes
         (zip/next
-          (if (= (zip/node loc) '__)
-            ; (do ..... einfügen für alle seq aus solution, löschen des alten)
-            (loop [l loc seq solution]
-              (if (empty? seq)
-                (-> l zip/remove zip/next)
-                (recur (zip/insert-left l (first seq)) (rest seq))
-                ))
-            loc))))))
+          (do
+            #_(print "Current: " (zip/node loc) "\n")
+            (if (= (zip/node loc) '__)
+              (do
+                #_(print "Replacing")
+                (loop [l loc seq solution]
+                  (if (empty? seq)
+                    (-> l zip/remove zip/next)
+                    (recur (zip/insert-left l (first seq)) (rest seq))
+                    )))
+              loc)))))))
 
 (defmacro ?? [template & solution]
+  #_(print (fill-in-solution template solution))
   (fill-in-solution template solution))
 
-;(defmacro ?? [template & solution]
-;  (print (fill-in-solution template solution))
-;  42)
